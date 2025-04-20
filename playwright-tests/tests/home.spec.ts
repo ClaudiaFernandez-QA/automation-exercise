7925e5ec18fdb136260aee9a8bff360fa79d5551
ClaudiaFernandez-QA <claudia.fernandez.qa@gmail.com>
2025-04-20 19:13:37 -0300

    tests(Home): validar sección Recommended Items y Footer

--- playwright-tests/tests/home.spec.ts
+++ playwright-tests/tests/home.spec.ts
@@ -68,7 +68,7 @@ test.describe("Funcionalidad: Home", () => {
         await expect(page.getByRole('heading', { name: 'Features Items' })).toBeVisible();
     });
 
-    test('La sección de productos contiene al menos un ítem visible', async ({page}) => {
+    test('La sección de productos contiene al menos un ítem visible', async ({ page }) => {
 
         const productContainer = page.locator('.features_items');
         const cardContent = productContainer.locator('.product-image-wrapper');
@@ -226,6 +226,75 @@ test.describe("Funcionalidad: Home", () => {
         }
     });
 
+    //Validaciones Funcionalidad: Recommended Items
+
+    test('La sección "Recommended Items" muestra productos con su estructura completa', async ({ page }) => {
+        const recommendedSection = page.locator('#recommended-item-carousel');
+
+        await test.step('Se visualiza el título "RECOMMENDED ITEMS"', async () => {
+            await expect(page.getByRole('heading', { name: 'recommended items' })).toBeVisible();
+        });
+
+        await test.step('Se muestra al menos un producto recomendado', async () => {
+            const products = recommendedSection.locator('.product-image-wrapper');
+            const quantityProducts = await products.count();
+            expect(quantityProducts).toBeGreaterThan(0);
+        });
+
+        await test.step('Validar estructura del primer producto visible', async () => {
+            const firstProduct = recommendedSection.locator('.product-image-wrapper').first();
+
+            await expect(firstProduct.locator('img')).toBeVisible();
+            await expect(firstProduct.locator('p')).toBeVisible();
+            await expect(firstProduct.locator('h2')).toBeVisible();
+            await expect(firstProduct.locator('.add-to-cart')).toBeVisible();
+
+        });
+    });
+
+
+    test('La sección "Recommended Items" permite navegar entre slides', async ({ page }) => {
+        const carrusel = page.locator('#recommended-item-carousel');
+        const rightArrow = carrusel.locator('.right');
+
+        const slideActive = carrusel.locator('.item.active .product-image-wrapper');
+        const productsBefore = await slideActive.allTextContents();
+
+        await test.step('Hacer clic en la flecha derecha para cambiar de slide', async () => {
+            await rightArrow.click();
+        });
+
+        await test.step('Validar que cambió el contenido del carrusel', async () => {
+            await expect.poll(async () => {
+                const newProducts = await carrusel.locator('.item.active .product-image-wrapper').allTextContents();
+
+                return newProducts;
+            }, {
+                timeout: 3000,
+            }).not.toEqual(productsBefore);
+        });
+    });
+
+
+    //Validación del Footer
+
+    test('Sección Footer: permite suscribirse correctamente', async ({ page }) => {
+        await test.step('Validar visualización del título "Subscription" y campo de email', async () => {
+          await expect(page.getByRole('heading', { name: 'Subscription' })).toBeVisible();
+          await expect(page.getByText('Get the most recent updates from our site and be updated your self...')).toBeVisible();
+          await expect(page.getByPlaceholder('Your email address')).toBeVisible();
+        });
+      
+        await test.step('Completar email y enviar formulario', async () => {
+          await page.getByPlaceholder('Your email address').fill('clau.tester@example.com');
+          await page.getByRole('button', { name: '' }).click();
+        });
+      
+        await test.step('Validar mensaje de éxito', async () => {
+          await expect(page.getByText('You have been successfully subscribed!')).toBeVisible();
+        });
+      });
+      
 
 
 });
